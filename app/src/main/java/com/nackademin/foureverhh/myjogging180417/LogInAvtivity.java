670a3f76c9b29b1a4f97a1b0096354408402c71b
foureverhh <foureverhh@gmail.com>
2018-04-17 10:38:18 +0200

    Initial commit

---
+++ app/src/main/java/com/nackademin/foureverhh/myjogging180417/LogInAvtivity.java
@@ -0,0 +1,13 @@
+package com.nackademin.foureverhh.myjogging180417;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LogInAvtivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in_avtivity);
+    }
+}
